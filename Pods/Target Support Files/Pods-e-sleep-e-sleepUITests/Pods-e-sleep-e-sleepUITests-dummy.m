0458b8eff251aaced63a17359a9272eeef0706f0
RenMatsushita <ren.git.x@gmail.com>
2020-01-18 16:48:00 +0900

    update git ignore

--- Pods/Target Support Files/Pods-e-sleep-e-sleepUITests/Pods-e-sleep-e-sleepUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_e_sleep_e_sleepUITests : NSObject
-@end
-@implementation PodsDummy_Pods_e_sleep_e_sleepUITests
-@end
--- Pods/Target Support Files/Pods-e-sleep/Pods-e-sleep-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_e_sleep : NSObject
-@end
-@implementation PodsDummy_Pods_e_sleep
-@end
