e0ae109191a76bcdf729c43cb14be3f73557c038
RenMatsushita <ren.git.x@gmail.com>
2020-01-20 22:00:43 +0900

    done alarm realm operator

--- e-sleep/Realm/Operator/AlarmRealmOperator.swift
+++ e-sleep/Realm/Operator/AlarmRealmOperator.swift
@@ -7,7 +7,61 @@
 //
 
 import Foundation
+import RxSwift
+import RealmSwift
 
-class AlarmRealmOperator {
+//realmとのやり取りのみ行う
+
+protocol AlarmRealmOperatorProtocol {
+    func set(alarm: Alarm)
+    func get() -> [Alarm]
+    func update(newAlarm: Alarm)
+    func delete(alarm: Alarm)
+}
+
+class AlarmRealmOperator: AlarmRealmOperatorProtocol {
+    func set(alarm: Alarm) {
+        do {
+            let realm = try Realm()
+            try realm.write {
+                realm.add(alarm)
+            }
+        } catch {
+           print(error)
+        }
+    }
     
+    func get() -> [Alarm] {
+        var alarms: [Alarm] = []
+        do {
+            let realm = try Realm()
+            _ = realm.objects(Alarm.self).map { alarms.append($0) }
+        } catch {
+            print(error)
+        }
+        return alarms
+    }
+    
+    func update(newAlarm: Alarm) {
+        do {
+            let realm = try Realm()
+            let alarm = realm.objects(Alarm.self).filter("notificationIdentifier == '\(newAlarm.notificationIdentifier)'").first
+            try realm.write {
+                alarm?.notificationIdentifier = newAlarm.notificationIdentifier
+            }
+        } catch {
+            print(error)
+        }
+    }
+    
+    func delete(alarm: Alarm) {
+        do {
+            let realm = try Realm()
+            try realm.write {
+                realm.delete(alarm)
+            }
+        } catch {
+            print(error)
+        }
+    }
 }
